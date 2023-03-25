4423d8a9a4bb067f78f006dfe7279d2c5b38324a
sanchitpasricha <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>
2023-03-25 13:58:42 +0530

    catch name error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,48 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+    env: process.env.NODE_ENV,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR 💣", err);
+
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
-  console.log(err.stack);
+  // console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
+  }
 };
--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,6 +1,6 @@
 module.exports = (fn) => {
   return (req, res, next) => {
-    fn(req, res, next).catch((err) => next(err));
+    fn(req, res, next).catch(next);
     //this will check the error in promise and redicrects to global error handler
   };
 };
