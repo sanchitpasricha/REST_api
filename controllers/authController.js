9a98260b78cc9137794f0cd1ccaf9a642920c97b
sanchitpasricha <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>
2023-03-29 00:23:48 +0530

    handling fields

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
+const appError = require("./../utils/appError");
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -20,5 +21,23 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  // email = req.body.email
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new appError("Please provide email and password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  console.log(user);
+
+  const token = "";
+  res.status(200).json({
+    status: "Success",
+    token,
   });
 });
